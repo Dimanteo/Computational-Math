4056bb1f28eaa16c3765cac21394df5cbf6a7956
Dimanteo <dikolobay@gmail.com>
2021-10-10 13:54:29 +0300

    [DataTable] Remove row/col ordering
    Removed ordering of columns and rows because such structure was not
    cache-friendly in most common use cases (addition, multiplication etc.)

--- DataTable.hpp
+++ DataTable.hpp
@@ -3,21 +3,19 @@
 #include <cstddef>
 #include <stdexcept>
 #include <utility>
+#include <cstring>
 
 namespace coma {
 
 template <typename T> class DTRowRef {
     T *m_row;
     size_t m_len;
-    size_t *m_order;
 
   public:
-    DTRowRef(T *row, size_t *order, size_t len) : m_row(row), m_len(len), m_order(order) {}
+    DTRowRef(T *row, size_t len) : m_row(row), m_len(len) {}
     T &operator[](size_t i) {
         assert(i < m_len && "index out of range");
-        assert(m_order);
-        size_t real_i = m_order[i];
-        return m_row[real_i];
+        return m_row[i];
     }
 };
 
@@ -25,8 +23,6 @@ template <typename Data_T> class DataTable {
     Data_T *m_data;
     size_t m_width;
     size_t m_height;
-    Data_T **m_orderRows;
-    size_t *m_orderCols;
 
   public:
     DataTable(size_t ncols, size_t nrows);
@@ -46,39 +42,33 @@ template <typename Data_T> class DataTable {
 
 template <typename T>
 DataTable<T>::DataTable(size_t width, size_t height) : m_width(width), m_height(height) {
-    m_data = new T[width *
-                   height](); // value-initialization casts to zero-initialization for number types
-    m_orderCols = new size_t[width];
-    m_orderRows = new T *[height];
-    for (size_t i = 0; i < width; i++) {
-        m_orderCols[i] = i;
-    }
-    for (size_t i = 0; i < height; i++) {
-        m_orderRows[i] = m_data + m_width * i;
-    }
+    // value-initialization casts to zero-initialization for number types
+    m_data = new T[width * height]();
 }
 
 template <typename T> DataTable<T>::~DataTable() {
     delete[](m_data);
-    delete[](m_orderCols);
-    delete[](m_orderRows);
 }
 
 template <typename T> DTRowRef<T> DataTable<T>::operator[](size_t row_i) {
     assert(row_i < m_height && "index out of range");
-    return DTRowRef<T>(m_orderRows[row_i], m_orderCols, m_width);
+    return DTRowRef<T>(m_data + row_i * m_width, m_width);
 }
 
-template <typename T> void DataTable<T>::swapRows(size_t l, size_t r) {
-    assert(l < m_height && "index out of range");
-    assert(r < m_height && "index out of range");
-    std::swap(m_orderRows[l], m_orderRows[r]);
+template <typename T> void DataTable<T>::swapRows(size_t lrow, size_t rrow) {
+    assert(lrow < m_height && "index out of range");
+    assert(rrow < m_height && "index out of range");
+    for (size_t col = 0; col < m_width; col++) {
+        std::swap((*this)[lrow][col], (*this)[rrow][col]);
+    }
 }
 
-template <typename T> void DataTable<T>::swapCols(size_t l, size_t r) {
-    assert(l < m_width && "index out of range");
-    assert(r < m_width && "index out of range");
-    std::swap(m_orderCols[l], m_orderCols[r]);
+template <typename T> void DataTable<T>::swapCols(size_t lcol, size_t rcol) {
+    assert(lcol < m_width && "index out of range");
+    assert(rcol < m_width && "index out of range");
+    for (size_t row = 0; row < m_height; row++) {
+        std::swap((*this)[row][lcol], (*this)[row][rcol]);
+    }
 }
 
 } // namespace coma
